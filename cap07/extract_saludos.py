faaa3e5dc14f313959925e4c8e6093c781637666
aniversarioperu <aniversarioperu1@gmail.com>
2014-02-05 22:25:21 +0200

    lastname comparison corrects for case-sensitive

--- cap07/extract_saludos.py
+++ cap07/extract_saludos.py
@@ -2,13 +2,16 @@
 from bs4 import BeautifulSoup
 import codecs
 import glob
+
+apellido = u"Julca"
+
 for filename in glob.glob("*html"):
     f = codecs.open(filename, "r", "latin1")
     html_doc = f.read()
     f.close()
     soup = BeautifulSoup(html_doc)
     for tag in soup.find_all("td", width="150"):
-        if 'Molina' in tag.text:
+        if apellido.lower() in tag.text.lower():
             prev = tag.previous_element
             prev2 = prev.previous_element
             prev3 = prev2.previous_element
